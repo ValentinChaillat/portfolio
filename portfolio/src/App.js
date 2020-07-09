1c3c42b3ffd75a6df132b2bf6dbe7e9de9647a9c
ValentinChaillat <vavanewcompte@hotmail.fr>
2020-07-09 15:50:57 +0200

    project ready to code

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar";
+import "./styles/index.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app col-12 row p-0 m-0">
+      <div className="bordure col-1 p-0 m-0"></div>
+      <div className="middle col-10 p-0 m-0">
+        <h1>Titre</h1>
+        <div className="navigation">
+          <Navbar />
+        </div>
+      </div>
+
+      <div className="bordure col-1 p-0 m-0"></div>
     </div>
   );
 }
---
+++ portfolio/src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="navbar col-12 row">
+      <p className="col-3">Element1</p>
+      <p className="col-3">Element2</p>
+      <p className="col-3">Element3</p>
+      <p className="col-3">Element4</p>
+    </div>
+  );
+}
+
+export default Navbar;
